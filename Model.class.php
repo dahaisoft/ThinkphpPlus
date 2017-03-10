e49e22ecca18723b832e7fc9a62c734fa0f66c96
dahai <haiwei.free@gmail.com>
2017-03-10 14:41:30 +0800

    First commit

---
+++ Model.class.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ThinkphpPlus;
+
+/**
+ *
+ */
+class Model extends \Think\Model
+{
+
+    /**
+     * auto默认时间
+     * @return datetime        Y-m-d
+     */
+    protected function date()
+    {
+        return date('Y-m-d');
+    }
+
+    /**
+     * auto默认时间
+     * @return datetime        Y-m-d H:i:s
+     */
+    protected function datetime()
+    {
+        return date('Y-m-d H:i:s');
+    }
+
+    /**
+     * auto空时默认时间
+     * @return datetime        Y-m-d H:i:s
+     */
+    protected function emptyDatetime($value)
+    {
+        return empty($value) ? date('Y-m-d H:i:s') : $value;
+    }
+
+    /**
+     * auto数据第一个为值
+     * @param array $value 数据
+     */
+    protected function setArrayFirst($value)
+    {
+        return is_array($value) ? $value[0] : $value;
+    }
+
+    /**
+     * auto数据第一个为值
+     * @param array $value 数据
+     */
+    protected function setDefault($value)
+    {
+        return $value;
+    }
+}
